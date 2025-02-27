cb90dd5fdbcb91691270fb30a7b0c7b25a257de3
nguyen van sy <nguyenvansy031999@gmail.com>
2025-02-27 22:15:07 +0700

    Complete the function of changing user information

--- src/routes/user.router.js
+++ src/routes/user.router.js
@@ -1,10 +1,11 @@
 import express from "express";
 import userController from "../controllers/user.controller.js";
 import { protect } from "../middleware/protect.middleware.js";
+import uploadCloud from "../common/multer/upload-cloud.multer.js";
 
 const userRouter = express.Router();
 
 userRouter.get("/getInfo", protect, userController.getInfo);
-userRouter.put("/editUserInfo", protect, userController.editUserInfo)
+userRouter.put("/editUserInfo", protect, uploadCloud.single("avatar"), userController.editUserInfo)
 
 export default userRouter;
--- src/services/user.service.js
+++ src/services/user.service.js
@@ -1,4 +1,5 @@
 import prisma from "../common/prisma/init.prisma.js";
+import { v2 as cloudinary } from "cloudinary";
 
 const userService = {
   getInfo: async (req) => {
@@ -18,9 +19,30 @@ const userService = {
     return user;
   },
   editUserInfo: async (req) => {
-    const { ho_ten, tuoi, anh_dai_dien } = req.body;
+    const { ho_ten, tuoi } = req.body;
 
-    const { user } = req;
+    const { user, file } = req;
+
+    // Configuration
+    cloudinary.config({
+      cloud_name: "dsti6aojz",
+      api_key: "374217273238878",
+      api_secret: "uIeIWGgOWSBjT7fBRx7frtK56kE", // Click 'View API Keys' above to copy your API secret
+    });
+
+    let uploadResult;
+
+    if (file) {
+      uploadResult = await new Promise((resolve) => {
+        cloudinary.uploader
+          .upload_stream({ folder: "images" }, (error, uploadResult) => {
+            return resolve(uploadResult);
+          })
+          .end(file.buffer);
+      });
+    }
+
+
     const updateNew = await prisma.nguoi_dung.update({
       where: {
         nguoi_dung_id: user.nguoi_dung_id,
@@ -28,7 +50,7 @@ const userService = {
       data: {
         ho_ten: ho_ten?.trim() ? ho_ten?.trim() : user.ho_ten,
         tuoi: Number.isInteger(tuoi) && tuoi > 0 ? tuoi : user.tuoi,
-        anh_dai_dien: anh_dai_dien ?? user.anh_dai_dien,
+        anh_dai_dien: uploadResult ? uploadResult.secure_url : user.anh_dai_dien,
       },
     });
 
